595af4854668b1ca4c123d11d77f8b42eb857b0a
Tal-el levi <kilek1751@gmail.com>
2020-06-10 14:32:44 +0300

    added DFSL

--- Heuristics.py
+++ Heuristics.py
@@ -46,27 +46,30 @@ class Heuristic:
                    abs(down_left + up_right + down_right + down + right - up - left - up_left),
                    abs(up_left + up_right + down_right + up + right - down - left - down_left))
 
-#
-#
+
+
+
 # def timing(func):
 #     import time
 #     def wrapper(*args, **kwargs):
 #         start_time = time.time()
 #         temp = func(*args, **kwargs)
 #         run_time = time.time() - start_time
-#         print(f'the function ran in {run_time}')
+#         if run_time > 0.001:
+#             print(f'the function ran in {run_time} and value{temp}')
 #         return temp
 #
 #     return wrapper
 
-# @timing
 
+# @timing
 def backtrack_out(problem, cell):
     player = problem.board[cell]
     problem.board[cell] = 0
     ret_val = inner_backtrack(problem, cell)
     problem.board[cell] = player
     return ret_val
+
 
 def inner_backtrack(problem, cell):
     if not problem.in_board(cell):
@@ -86,18 +89,42 @@ def inner_backtrack(problem, cell):
     return ret_value
 
 
-class Heuristic2:
-    def estimate(self, problem, playing_agent):
-        dist = problem.get_dist_from_rival()
-        if dist <= 10:
-            return 100 - dist + problem.get_nr_of_neighbor_unvisited_cell(1)
+# @timing
+def dfsl(problem, cell, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(cell)
 
-        escape_route = backtrack_out(problem, problem.loc)
-        if not escape_route:
+    considered_cells = [(move[0] + cell[0], move[1] + cell[1]) for move in problem.get_viable_moves()]
+    legal_cells = [cell for cell in considered_cells if problem.in_board(cell)
+                   and cell not in visited
+                   and problem.board[cell] == 0]
+
+    for curr_cell in legal_cells:
+        # check if we escaped the "box"
+        if abs(problem.loc[0] - curr_cell[0]) > 3 or abs(problem.loc[1] - curr_cell[1]) > 3:
+            return True
+        elif dfsl(problem, curr_cell, visited):
+            return True
+    return False
+
+
+class Heuristic2:
+    war_radius = 7
+    def estimate(self, problem, playing_agent):
+        dist_from_rival = problem.get_dist_from_rival()
+        if dist_from_rival <= self.war_radius:
+            return 100 - round((dist_from_rival/self.war_radius)*3) + problem.get_nr_of_neighbor_unvisited_cell(1)
+
+        # escape_route = backtrack_out(problem, problem.loc)
+        escape_route2 = dfsl(problem, problem.loc)
+        if not escape_route2:
             return -100
         count = 0
         for i in range(-3, 4):
             for j in range(-3, 4):
+                if (i == 0 and j == -1 or j == 1) or (j == 0 and i == 1 or i == -1):
+                    continue
                 cell = (problem.loc[0] + i, problem.loc[1] + j)
                 if problem.in_board(cell):
                     if problem.board[cell] == -1:
@@ -106,5 +133,7 @@ class Heuristic2:
                         count -= 1
                 else:
                     count += 1
-        return count
-
+        max_dist = len(problem.board) + len(problem.board[0])
+        curr_dist_from_start = problem.manhattan_dist_from_start()
+        value = count - round((curr_dist_from_start / max_dist)) * 30
+        return value
